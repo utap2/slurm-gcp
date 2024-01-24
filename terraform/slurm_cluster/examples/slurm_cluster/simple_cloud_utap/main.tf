4c81fa938bbe87c1b2a804917f0c7f7e47cc2227
utap2 <130136775+utap2@users.noreply.github.com>
2024-01-24 11:16:13 +0200

    Update main.tf

--- terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
+++ terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
@@ -72,7 +72,7 @@ locals {
         remote_mount  = "utap-data-devops-279708"
         local_mount   = "/data"
         fs_type       = "gcsfuse"
-        mount_options = "file_mode=664,dir_mode=775,allow_other"
+        mount_options = "rw,_netdev,user,file_mode=777,dir_mode=777,allow_other"
      }]
     },
   ]
