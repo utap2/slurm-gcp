be4a0bdf6deeb31af7a43ae23842f70b4bb2f5db
utap2 <130136775+utap2@users.noreply.github.com>
2024-02-13 11:45:16 +0200

    Update main.tf

--- terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
+++ terraform/slurm_cluster/examples/slurm_cluster/simple_cloud_utap/main.tf
@@ -70,15 +70,15 @@ locals {
         Default = "YES"
       }
       partition_name    = "compute"
-      partition_nodeset = [local.nodeset[0].nodeset_name]#    
-      network_storage      = [{
-        server_ip     = "none"
-        remote_mount  = "BUCKET_NAME"
-        local_mount   = "/data"
-        fs_type       = "gcsfuse"
-        #mount_options = "rw,_netdev,user,file_mode=777,dir_mode=777,allow_other"
-        mount_options = "rw,_netdev,user,file_mode=777,dir_mode=777"
-     }]
+      partition_nodeset = [local.nodeset[0].nodeset_name]
+     # network_storage      = [{
+     #  server_ip     = "none"
+     #   remote_mount  = "BUCKET_NAME"
+     #   local_mount   = "/data"
+     #   fs_type       = "gcsfuse"
+     #  #mount_options = "rw,_netdev,user,file_mode=777,dir_mode=777,allow_other"
+     #   mount_options = "rw,_netdev,user,file_mode=777,dir_mode=777"
+     #}]
     },
   ]
 }
